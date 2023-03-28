4ebfc579e53366589ee89fe1b25a7ef17d84e433
IFV_LongTH <long-th@if-vietnam.com>
2023-03-28 09:29:06 +0700

    done

--- js/app.js
+++ js/app.js
@@ -6,20 +6,25 @@ window.addEventListener("DOMContentLoaded", () => {
     const audioFiles = ['./audio/01_a.mp3', './audio/02_i.mp3', './audio/03_u.mp3', './audio/04_e.mp3', './audio/05_o.mp3'];
     const buttons = document.querySelectorAll('.play-btn');
     buttons.forEach((button) => {
+
       button.addEventListener('click', () => {
         const index = button.dataset.index;
+        buttons.forEach((button) => {
+          button.classList.remove('playing');
+        });
         if (currentAudio && !currentAudio.paused) {
             currentAudio.pause();
             currentAudio.currentTime = 0;
         }
         currentAudio = new Audio(audioFiles[index]);
         currentAudio.play();
-        downloadButton.disabled = false;
         stopButton.disabled = false;
         button.classList.add('playing')
+
         currentAudio.addEventListener('ended', function() {
           if (currentAudio && !currentAudio.paused) {
             console.log('111');
+            button.classList.remove('playing')
           }
           console.log('222');
           for (let step = 0; step < buttons.length; step++) {
@@ -30,17 +35,16 @@ window.addEventListener("DOMContentLoaded", () => {
     });
     
 
-    //dowload
     let currentAudio = null;
-    const downloadButton = document.querySelector('#download-btn');
-    downloadButton.addEventListener('click', () => {
-      const a = document.createElement('a');
-      a.href = currentAudio.src;
-      a.download = currentAudio.src.split('/').pop();
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-    });
+    // const downloadButton = document.querySelector('#download-btn');
+    // downloadButton.addEventListener('click', () => {
+    //   const a = document.createElement('a');
+    //   a.href = currentAudio.src;
+    //   a.download = currentAudio.src.split('/').pop();
+    //   document.body.appendChild(a);
+    //   a.click();
+    //   document.body.removeChild(a);
+    // });
 
     //stop
     const stopButton = document.querySelector('#stop-btn');
@@ -48,8 +52,7 @@ window.addEventListener("DOMContentLoaded", () => {
         currentAudio.pause();
         currentAudio.currentTime = 0;
         stopButton.disabled = true;
-        downloadButton.disabled = true;
-        console.log('dung');
+        // downloadButton.disabled = true;
         buttons.forEach((button) => {
           buttons.forEach((button) => {
             button.classList.remove('playing')
